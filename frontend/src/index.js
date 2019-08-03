b8ff9427ce11e7d9105c8ee3a9883246db329a53
Brian Vaughn <bvaughn@fb.com>
2019-08-03 10:33:15 -0700

    Replaced legacy ReactDOM render with batch API

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { unstable_createRoot as createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-const frame = document.getElementById('sandbox');
-
-frame.onload = () => {
-  // It is now safe to postMessage data to the frame.
-};
-
-/* This enables the frontend to inject code into the iframe, even though it is sandboxed.
-function evalInFrame(code) {
-  frame.contentWindow.postMessage({
-    type: 'eval',
-    payload: code
-  }, '*');
-}
-*/
+const root = createRoot(document.getElementById('root'))
+root.render(<App />);
