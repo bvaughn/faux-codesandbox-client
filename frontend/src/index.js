64925bfe648b53a203e4b3b934909968e8769899
Brian Vaughn <bvaughn@fb.com>
2019-08-03 10:51:13 -0700

    Replaced createRoot with createSyncRoot

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { unstable_createRoot as createRoot } from 'react-dom';
+import { unstable_createSyncRoot as createSyncRoot } from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = createRoot(document.getElementById('root'))
+// It's important to use either createRoot() or createSyncRoot() for DevTools UI!
+// Sync root is a safer incremental step if the surrounding app is not yet concurrent mode compatible.
+const root = createSyncRoot(document.getElementById('root'))
 root.render(<App />);
