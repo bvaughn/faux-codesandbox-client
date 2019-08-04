3fd891ad8e06adb136efaec56b4349265bc9ee41
Brian Vaughn <bvaughn@fb.com>
2019-08-04 10:21:19 -0700

    Remove 'message' listener when done

--- backend/src/setupReactDevTools.js
+++ backend/src/setupReactDevTools.js
@@ -1,13 +1,17 @@
 import { activate } from 'react-devtools-inline/backend';
 
-// Wait for the frontend Store to be ready before initializing the backend.
-// This way the Store doesn't miss any tree operations.
-window.addEventListener('message', ({ data }) => {
+const listener = ({ data }) => {
   switch (data.type) {
     case 'activate':
+      window.removeEventListener('message', listener);
+
+      // Wait for the frontend Store to be ready before initializing the backend.
+      // This way the Store doesn't miss any tree operations.
       activate(window);
       break;
     default:
       break;
   }
-});
+};
+
+window.addEventListener('message', listener);
